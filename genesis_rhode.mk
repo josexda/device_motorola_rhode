1e58297838785aa846101755c0d154b3dfedf89b
josexda <josegustavo6@protonmail.com>
2025-04-18 19:40:00 +0200

    rhode: init Genesis
    Change-Id: I55ffc0c94acf3927ab426334cf4f8198c4efd437

--- genesis_rhode.mk
+++ genesis_rhode.mk
@@ -10,11 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Genesis stuff.
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
+GENESIS_MAINTAINER := josexda
+GENESIS_OFFICIAL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_rhode
+PRODUCT_NAME := genesis_rhode
 PRODUCT_DEVICE := rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
